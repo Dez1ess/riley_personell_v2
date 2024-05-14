bee92280ac4ffec6fa4bb394dac90c3837438328
KMD <KMD@d.c>
2024-05-14 17:19:02 +0300

    update responsive

--- src/js/main.js
+++ src/js/main.js
@@ -8,15 +8,35 @@ import './libs/countUp.lib.js';
 
 import Swiper, {Navigation, Scrollbar} from 'swiper';
 
-const swiper = new Swiper(".swiper", {
+export const sectorsSwiper = new Swiper(".swiper", {
   slidesPerView: 3,
+  slidesOffsetAfter: 85,
   spaceBetween: 16,
+  loop: true,
+  slideActiveClass: "swiper-slide-active",
+  slideBlankClass: "swiper-slide-blank",
+  slideToClickedSlide: true,
+  slidesOffsetBefore: 0,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.1,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 16,
+    },
+  },
   modules: [Navigation, Scrollbar],
   scrollbar: {
     el: ".swiper-scrollbar",
   },
   navigation: {
-    nextEl: ".swipe-button-next",
-    prevEl: ".swipe-button-previous",
+    nextEl: ".sectors__swipe-button-next",
+    prevEl: ".sectors__swipe-button-prev",
   },
 });
