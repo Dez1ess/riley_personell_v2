78b69c7ba5df315d890b791082838bce500844b9
Dez1ess <davidkulmohammad@gmail.com>
2024-05-16 15:50:17 +0300

    add respoonsive menu

--- src/js/main.js
+++ src/js/main.js
@@ -40,3 +40,32 @@ export const sectorsSwiper = new Swiper(".swiper", {
     prevEl: ".sectors__swipe-button-prev",
   },
 });
+
+
+/* BURGER MENU LINK COLLAPSE */
+document.addEventListener('DOMContentLoaded', function() {
+  const advancedLink = document.querySelector('.burger-menu__link-advanced__title');
+  const advancedContent = document.querySelector('.burger-menu__link-advanced__content');
+  const burger = document.querySelector('.burger');
+  const burgerMenu = document.querySelector('.burger-menu');
+  const closeButton = document.querySelector('.burger-menu__close');
+
+  // Function to open the burger menu
+  function openBurgerMenu() {
+    burgerMenu.classList.add('open');
+  }
+
+  // Function to close the burger menu
+  function closeBurgerMenu() {
+    burgerMenu.classList.remove('open');
+  }
+
+  burger.addEventListener('click', openBurgerMenu);
+
+  closeButton.addEventListener('click', closeBurgerMenu);
+
+  advancedLink.addEventListener('click', function() {
+    advancedLink.classList.toggle('expanded');
+    advancedContent.classList.toggle('expanded');
+  });
+});
