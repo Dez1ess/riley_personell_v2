15ce61dd62330394bd6deddf5a9d7cbf7f0bee12
Dez1ess <davidkulmohammad@gmail.com>
2024-05-20 01:22:04 +0300

    finished website

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,13 @@ import './components/inputs.component.js';
 import './libs/countUp.lib.js';
 import Swiper, { Navigation, Scrollbar } from 'swiper';
 
-export const sectorsSwiper = new Swiper(".swiper", {
+/* SECTORS SWIPER */
+export const sectorsSwiper = new Swiper('.sectors__swiper', {
   slidesPerView: 3,
   spaceBetween: 16,
   loop: true,
-  slideActiveClass: "swiper-slide-active",
-  slideBlankClass: "swiper-slide-blank",
+  slideActiveClass: 'swiper-slide-active',
+  slideBlankClass: 'swiper-slide-blank',
   slideToClickedSlide: true,
   breakpoints: {
     320: {
@@ -28,18 +29,66 @@ export const sectorsSwiper = new Swiper(".swiper", {
   },
   modules: [Navigation, Scrollbar],
   scrollbar: {
-    el: ".swiper-scrollbar",
+    el: '.swiper-scrollbar',
   },
   navigation: {
-    nextEl: ".sectors__swipe-button-next",
-    prevEl: ".sectors__swipe-button-prev",
+    nextEl: '.sectors__swipe-button-next',
+    prevEl: '.sectors__swipe-button-prev',
   },
 });
 
+/* CLIENTS SWIPER */
+export const clientsSwiper1 = new Swiper('.schools__swiper1', {
+  spaceBetween: 12,
+  slidesPerView: 'auto',
+  breakpoints: {
+    320: {
+      slidesPerView: 1.8,
+    },
+    768: {
+      slidesPerView: 'auto',
+    },
+    1280: {
+      slidesPerView: 'auto',
+    },
+  },
+});
+
+export const clientsSwiper2 = new Swiper('.schools__swiper2', {
+  spaceBetween: 12,
+  slidesPerView: 'auto',
+  breakpoints: {
+    320: {
+      slidesPerView: 1.8,
+    },
+    768: {
+      slidesPerView: 'auto',
+    },
+    1280: {
+      slidesPerView: 'auto',
+    },
+  },
+});
+
+/* OPPORTUNITIES SWIPER */
+export const opportunitiesSwiper = new Swiper('.opportunities__swiper', {
+  spaceBetween: 20,
+  slidesPerView: 'auto',
+  modules: [Navigation],
+  navigation: {
+    nextEl: '.opportunities__swipe-button-next',
+    prevEl: '.opportunities__swipe-button-prev',
+  },
+})
+
 /* BURGER MENU LINK COLLAPSE */
-document.addEventListener('DOMContentLoaded', function() {
-  const advancedLink = document.querySelector('.burger-menu__link-advanced__title');
-  const advancedContent = document.querySelector('.burger-menu__link-advanced__content');
+document.addEventListener('DOMContentLoaded', function () {
+  const advancedLink = document.querySelector(
+    '.burger-menu__link-advanced__title'
+  );
+  const advancedContent = document.querySelector(
+    '.burger-menu__link-advanced__content'
+  );
   const burger = document.querySelector('.burger');
   const burgerMenu = document.querySelector('.burger-menu');
   const closeButton = document.querySelector('.burger-menu__close');
@@ -56,7 +105,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   burger.addEventListener('click', openBurgerMenu);
   closeButton.addEventListener('click', closeBurgerMenu);
-  advancedLink.addEventListener('click', function() {
+  advancedLink.addEventListener('click', function () {
     advancedLink.classList.toggle('expanded');
     advancedContent.classList.toggle('expanded');
   });
@@ -68,19 +117,19 @@ document.addEventListener('DOMContentLoaded', function() {
 window.addEventListener('resize', handleResize);
 
 function handleResize() {
-  const slides = document.querySelectorAll('.swiper-slide');
+  const slides = document.querySelectorAll('.sectors__swiper-slide');
   const activeSlide = document.querySelector('.swiper-slide-active');
 
   if (slides.length > 0 && activeSlide !== null) {
     if (window.innerWidth < 768) {
       // Mobile viewport
-      slides.forEach(slide => {
+      slides.forEach((slide) => {
         slide.classList.add('swiper-slide-mobile-active');
       });
       activeSlide.classList.remove('swiper-slide-active');
     } else {
       // Desktop viewport
-      slides.forEach(slide => {
+      slides.forEach((slide) => {
         slide.classList.remove('swiper-slide-mobile-active');
       });
       activeSlide.classList.add('swiper-slide-active');
